3d01010eb757406a35ab98c7ffee84c928274e37
cataramocini-stack <justinianoconstantinogrande@gmail.com>
2026-03-13 03:05:41 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -50,14 +50,23 @@ session = requests.Session()
 session.verify = False
 
 session.headers.update({
-"User-Agent":
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-"(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
-"Accept":
-"text/html,application/xhtml+xml,application/xml;q=0.9,"
-"image/avif,image/webp,*/*;q=0.8",
-"Accept-Language":"pt-BR,pt;q=0.9,en-US;q=0.8",
-"Connection":"keep-alive",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Sec-Ch-Ua": '"Google Chrome";v="146", "Chromium";v="146", "Not=A?Brand";v="99"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Ch-Ua-Platform-Version": '"10.0.0"',
+    "Sec-Ch-Ua-Full-Version-List": '"Google Chrome";v="146.0.7680.72", "Chromium";v="146.0.7680.72", "Not=A?Brand";v="99.0.0.0"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "Connection": "keep-alive",
+    # "DNT": "1",                  # opcional, mas alguns sites usam pra fingerprint
+    # "Priority": "u=0, i",        # Chrome moderno manda isso em 2026 (prioridade alta)
 })
 
 # ================= UTIL =================
